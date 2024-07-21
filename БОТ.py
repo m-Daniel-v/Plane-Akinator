ca3c7273580345cce542e214f6ae9ddfaad1b790
m-Daniel-v <m.daniel.v.05@gmail.com>
2024-07-21 16:53:23 +0300

    Add files via upload

--- БОТ.py
+++ БОТ.py
@@ -3,7 +3,7 @@ from telebot import types
 import sqlite3
 
 
-bot = telebot.TeleBot('6754617208:AAHIpYXWjXB6PYuiKl0v5MtWBXr_KQP_2vs')
+bot = telebot.TeleBot('API_TOKEN')
 
 
 def load_data(db_file):
@@ -26,7 +26,7 @@ def preprocess_data(data):
             'construction_classification': row[5].lower().split(';') if row[5] else [],
             'engine_type': row[6].lower().split(';') if row[6] else [],
             'flight_range_classification': row[7].lower().split(';') if row[7] else [],
-            'number_of_engines': str(row[8]),  # Преобразуем в строку для единообразия
+            'number_of_engines': str(row[8]),  # Преобразуем в строку
             'wing_location_classification': row[9].lower().split(';') if row[9] else [],
             'fuselage_type_classification': row[10].lower().split(';') if row[10] else [],
             'chassis_type_classification': row[11].lower().split(';') if row[11] else [],
@@ -36,21 +36,20 @@ def preprocess_data(data):
     return processed_data
 
 
-# Функция для генерации вопросов на основе классификаций
 def generate_question(classification, column_name):
     questions = {
         'classification_role': 'Это {} самолёт?',
-        'subclassification_role': 'Это {} ?',
+        'subclassification_role': 'Это {}?',
         'aerodynamic_balance_scheme': 'Это самолёт {} по своей аэродинамической балансировочной схеме?',
-        'construction_classification': 'Это {} ?',
+        'construction_classification': 'Это {}?',
         'engine_type': 'Этот самолёт имеет {} тип двигателя?',
-        'flight_range_classification': 'Это самолёт {} ?',
+        'flight_range_classification': 'Это самолёт {}?',
         'number_of_engines': 'У него {} двигателей?',
-        'wing_location_classification': 'Это {} ?',
+        'wing_location_classification': 'Это {}?',
         'fuselage_type_classification': 'Это {} самолёт?',
         'chassis_type_classification': 'Этот самолёт имеет {} шасси?',
-        'tail_type_and_location_classification': 'Этот самолёт имеет {} ?',
-        'engine_location_classification': 'У этого самолёта двигатели расположены {} ?'
+        'tail_type_and_location_classification': 'Этот самолёт имеет {}?',
+        'engine_location_classification': 'У этого самолёта двигатели расположены {}?'
     }
     return questions[column_name].format(classification)
 
@@ -71,8 +70,11 @@ def reset_game(message, restart=False):
         'current_data': processed_data,
         'yes_answers': [],
         'no_answers': [],
-        'asked_columns': set()
+        'asked_columns': set(),
+        'last_question': False
     }
+    if user_id in new_aircraft_data:
+        new_aircraft_data.pop(user_id)  # Удаляем промежуточные данные о новом самолете
     if restart:
         bot.send_message(user_id,
                          "Игра перезапущена. Давайте начнем угадывать самолёт заново. Отвечайте на вопросы да или нет.")
@@ -96,6 +98,11 @@ def ask_question(message):
     if not current_data:
         bot.send_message(user_id, "Не удалось определить самолёт.")
         request_new_aircraft_data(user_id)
+        return
+
+    if len(current_data) == 1 and not state['last_question']:
+        bot.send_message(user_id, f"Это {current_data[0]['name'].title()}?")
+        state['last_question'] = True
         return
 
     question_order = [
@@ -159,7 +166,7 @@ def handle_answer(message):
     user_id = message.chat.id
 
     if user_id not in user_state and user_id not in new_aircraft_data:
-        bot.send_message(user_id, "Начните с команды /start.")
+        bot.send_message(user_id, "Нажмите /start.")
         return
 
     if message.text.lower() == 'перезапуск':
@@ -181,7 +188,7 @@ def handle_answer(message):
     state = user_state[user_id]
 
     if 'current_classification' not in state:
-        bot.send_message(user_id, "Начните с команды /start.")
+        bot.send_message(user_id, "Нажмите /start.")
         return
 
     answer = message.text.strip().lower()
@@ -189,16 +196,25 @@ def handle_answer(message):
     column_key = state['current_column']
 
     if answer == 'да':
+        if state['last_question']:
+            bot.send_message(user_id, f"Ура, я угадал! Вы загадали самолёт: {state['current_data'][0]['name'].title()}")
+            user_state.pop(user_id)
+            return
         state['yes_answers'].append((column_key, classification))
         state['current_data'] = [entry for entry in state['current_data'] if classification in entry[column_key]]
     elif answer == 'нет':
+        if state['last_question']:
+            bot.send_message(user_id, "Не удалось определить самолёт.")
+            request_new_aircraft_data(user_id)
+            user_state.pop(user_id)
+            return
         state['no_answers'].append((column_key, classification))
         state['current_data'] = [entry for entry in state['current_data'] if classification not in entry[column_key]]
-        state['asked_columns'].discard(column_key)  # Удаляем колонку из списка заданных вопросов
+        state['asked_columns'].discard(column_key)  # Удаляем колонку из списка заданных вопросов, если ответ был "нет"
 
-    if len(state['current_data']) == 1:
-        bot.send_message(user_id, f"Ура, я угадал! Вы загадали самолёт: {state['current_data'][0]['name'].title()}")
-        user_state.pop(user_id)
+    if len(state['current_data']) == 1 and not state['last_question']:
+        bot.send_message(user_id, f"Это {state['current_data'][0]['name'].title()}?")
+        state['last_question'] = True
         return
     elif len(state['current_data']) == 0:
         bot.send_message(user_id, "Не удалось определить самолёт.")
@@ -209,6 +225,7 @@ def handle_answer(message):
     ask_question(message)
 
 
+# Функция для сохранения новых данных о самолете
 def save_new_aircraft_data(answers):
     conn = sqlite3.connect('aircrafts.db')
     cursor = conn.cursor()
